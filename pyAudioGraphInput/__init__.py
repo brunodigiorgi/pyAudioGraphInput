ffd218e9877e8833e2fc5b09dbdec2daa50cba63
Bruno Di Giorgi <bruno@brunodigiorgi.it>
2017-04-08 18:39:09 +0200

    readme

--- pyAudioGraphInput/__init__.py
+++ pyAudioGraphInput/__init__.py
@@ -1,3 +1,7 @@
-from .LeapMotionNode import LeapMotionNode
+try:
+    from .LeapMotionNode import LeapMotionNode
+except ImportError:
+    print("leap motion node not available, probably missing SDK")
+
 from .ContinuousScaleNode import ContinuousScaleNode
 from . import continuous_mappings as contmap
